c7dff4edd4ca825a9acc39150f4bb4922f2c64df
NKUBITO <emmanuelnkubito2@gmail.com>
2025-04-30 11:07:34 +0200

    change has made

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -228,9 +228,9 @@ const AdminDashboard: React.FC = () => {
                   fill="#8884d8"
                   label
                 >
-                  {roleDistributionData.map((entry, index) => (
+                    {roleDistributionData.map((_, index) => (
                     <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                  ))}
+                    ))}
                 </Pie>
                 <Legend />
               </PieChart>
